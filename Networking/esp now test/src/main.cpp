8b18a44934052394544e37ee6b2e12669142ed15
zhu30844 <162306639+zhu30844@users.noreply.github.com>
2024-08-22 18:13:26 +0800

    Update message structure

--- Networking/esp now test/src/main.cpp
+++ Networking/esp now test/src/main.cpp
@@ -1,50 +1,125 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <espnow.h>
+#define MAC_ADDR_SIZE 6
+#define SHOOTER_ID_SIZE 6
+//#define DEBUG
 uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-// put function declarations here:
+uint score = 0;
 typedef struct struct_message
 {
-    char a[32];
+    char macLocal[MAC_ADDR_SIZE];
+    char shooterID[SHOOTER_ID_SIZE];
 
 } struct_message;
 
-// Create a struct_message called shotted
-struct_message shotted;
+struct_message message;
 
+#ifdef DEBUG
 byte sendBuffer[255];
+byte sendStr[] = {'h', 'e', 'l', 'l', 'o'};
+#endif
 
-// Emulates a broadcast
-void broadcast(const byte *message, int stringLen)
-{	
-	
+// Function prototypes
+/* Broadcast Function */
+void broadcast(const uint8_t *message, int stringLen);
+/* RecvCallback Function */
+void RecvCallback(uint8_t *senderMAC, uint8_t *incomingData, uint8_t len);
+/* SentCallback Function */
+void SentCallback(uint8_t *mac_addr, uint8_t sendStatus);
+void Freeze();
+void Goal();
+
+void setup()
+{
+    // Init ESP
+    Serial.begin(115200);
+    Serial.println();
+    Serial.print("ESP Board MAC Address:  ");
+    Serial.println(WiFi.macAddress());
+    // Init message struct
+    strcpy(message.macLocal, WiFi.macAddress().c_str());
+    // Init motor board
+    
+    // Init ESP-NOW
+    WiFi.mode(WIFI_STA);
+    if (esp_now_init() != 0)
+    {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+    esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+    esp_now_register_recv_cb(RecvCallback);
+    esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
+    //
+
+}
+
+void loop()
+{
+#ifdef DEBUG
+    int sendBytes = Serial.available();
+    if (sendBytes > 0)
+    {
+        for (int i = 0; i < 255; i++)
+            sendBuffer[i] = 0;
+        int writePos = 0;
+        while (Serial.available() != 0)
+        {
+            sendBuffer[writePos] = Serial.read();
+            writePos++;
+        }
+        Serial.print("Broadcasting [");
+        for (int i = 0; i < writePos; i++)
+        {
+            Serial.print(sendBuffer[i]);
+            if (i != writePos - 1)
+                Serial.print(", ");
+        }
+        Serial.println("]");
+        broadcast(sendBuffer, writePos);
+    }
+    broadcast(sendStr, 5);
+#endif
+    // waiting for interruption
+    broadcast((uint8_t *)&message, sizeof(message));
+    delay(500);
+}
+void broadcast(const uint8_t *message, int stringLen)
+{
     // Broadcast a message to every device in range
-    // Send to all
     int sendStatus = esp_now_send(broadcastAddress, (u8 *)message, stringLen);
     // Print results to serial monitor
     if (sendStatus == 0)
-    {
         Serial.println("Delivery success");
-    }
     else
-    {
         Serial.println("Delivery fail");
-    }
     Serial.println();
 }
 
-void RecvCallback(uint8_t *mac, uint8_t *incomingData, uint8_t len)
+void RecvCallback(uint8_t *senderMAC, uint8_t *incomingData, uint8_t len)
 {
-    Serial.print("Recived\n");
-    for(uint8_t i = 0; i < len ; i++)
+    score++;
+#ifdef DEBUG
+    Serial.println("Recived from ");
+    String senderMAC_s;
+    if (sscanf(senderMAC_s.c_str(), "%02X:%02X:%02X:%02X:%02X:%02X", &senderMAC[0], &senderMAC[1], &senderMAC[2], &senderMAC[3], &senderMAC[4], &senderMAC[5]) != 6)
     {
-        char tmp = *(incomingData+i);
+        Serial.println("ERROR while fetch mac address");
+    }
+    else
+    {
+        Serial.println(senderMAC_s);
+    }
+    for (uint8_t i = 0; i < len; i++)
+    {
+        char tmp = *(incomingData + i);
         Serial.print(tmp);
     }
     Serial.print("\n");
-    
+#endif
 }
-/*
+
 void SentCallback(uint8_t *mac_addr, uint8_t sendStatus)
 {
 
@@ -57,65 +132,4 @@ void SentCallback(uint8_t *mac_addr, uint8_t sendStatus)
     {
         Serial.println("Delivery fail");
     }
-}
-*/
-void setup()
-{
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-    Serial.println();
-    Serial.print("ESP Board MAC Address:  ");
-    Serial.println(WiFi.macAddress());
-    WiFi.mode(WIFI_STA);
-
-    // Init ESP-NOW
-    if (esp_now_init() != 0)
-    {
-        Serial.println("Error initializing ESP-NOW");
-        return;
-    }
-
-    // Once ESPNow is successfully Init, we will register for recv CB to
-    // get recv packer info
-    // esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
-    esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
-    esp_now_register_recv_cb(RecvCallback);
-    esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_COMBO, 1, NULL, 0);
-    //broadcast("shotted by ", );
-}
-
-void loop()
-{
-	
-	int sendBytes = Serial.available();
-	if (sendBytes > 0) {
-		
-		for (int i = 0; i < 255; i ++) {
-			sendBuffer[i] = 0;
-		}
-		
-		int writePos = 0;
-		while (Serial.available() != 0) {
-			sendBuffer[writePos] = Serial.read();
-			writePos ++;
-		}
-		
-		
-		Serial.print("Broadcasting [");
-		for (int i = 0; i < writePos; i ++) {
-			Serial.print(sendBuffer[i]);
-			if (i != writePos - 1) {
-				Serial.print(", ");
-			}
-			
-		}
-		Serial.println("]");
-
-		broadcast(sendBuffer, writePos);
-		
-		
-	}
-	//broadcast("lol");
-	delay(500);
-    // put your main code here, to run repeatedly:
 }
