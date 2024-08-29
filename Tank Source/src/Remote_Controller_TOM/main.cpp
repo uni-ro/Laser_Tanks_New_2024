35a8a52e0cf848b4e443b80cca6ccab5e8a9229d
TSK7777 <103026807+TSK7777@users.noreply.github.com>
2024-08-29 20:18:01 +0800

    Rangeaaaaa

--- Tank Source/src/Remote_Controller_TOM/main.cpp
+++ Tank Source/src/Remote_Controller_TOM/main.cpp
@@ -1,10 +1,19 @@
 #include <Arduino.h>
 
-char string[256];
-
 
 // put function declarations here:
 int myFunction(int, int);
+
+int customMap(int value, int fromLow, int center, int fromHigh, int toLow, int centerOutput, int toHigh) {
+  if (value < center) {
+    // Map from the lower range
+    return map(value, fromLow, center, toLow, centerOutput);
+  } else {
+    // Map from the upper range
+    return map(value, center, fromHigh, centerOutput, toHigh);
+  }
+}
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,13 +24,30 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  uint16_t adc_val1 = analogRead(GPIO_NUM_32);
-  uint16_t adc_val2 = analogRead(GPIO_NUM_33);
+  uint16_t adc_valX = analogRead(GPIO_NUM_32);
+  uint16_t adc_valY = analogRead(GPIO_NUM_33);
+  int centerX = 2890;
+  int centerY = 2890;
+  int threshold = 50;
+
+  int scaledX = customMap(adc_valX, 0, 2890, 4095, -100, 0, 100);
+  int scaledY = customMap(adc_valY, 0, 2890, 4095, -100, 0, 100);
+
+  if( abs(scaledX) < 5 )
+  {
+    scaledX = 0;
+  }
+  
+  if( abs(scaledY) < 5 )
+  {
+    scaledY = 0;
+  }
+
   Serial.print("X: ");
-  Serial.print(adc_val1);
+  Serial.print(scaledX);
   Serial.print(' ');
   Serial.print("Y: ");
-  Serial.print(adc_val2);
+  Serial.print(scaledY);
   Serial.print("\n");
 
   delay(50);
