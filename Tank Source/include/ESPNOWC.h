653f741552f6490324fe12baa8d8ea3b693bc0bc
zhu30844 <162306639+zhu30844@users.noreply.github.com>
2024-09-12 19:18:32 +0800

    Create ESPNOWC.h

---
+++ Tank Source/include/ESPNOWC.h
@@ -0,0 +1,375 @@
+#ifndef __ESP_NOW_UTIL__
+#define __ESP_NOW_UTIL__
+
+#define MAC_ADDR_SIZE 12
+#define SHOOTER_ID_SIZE 1
+#define PLATFORM 32
+#define SIGNCHANNEL 2
+// general headers
+#include <Arduino.h>
+
+uint8_t callbackLock = 0;
+enum deviceType
+{
+    TANK,
+    CONTROLLER,
+    GAMESERVER
+};
+
+enum paringStatus
+{
+    PAIRING,
+    PAIRING_SERVER,
+    PAIRED_SERVER,
+    PAIRING_CONTROLLER,
+    PAIRED_CONTROLLER,
+    PAIRING_TANK,
+    PAIRED_TANK
+};
+
+// structure for pairing, the tank itself
+typedef struct struct_pairing
+{
+    deviceType id;
+    uint8_t macAddr[6];
+} struct_pairing;
+
+typedef struct struct_controller
+{
+    uint8_t command[10];
+    uint8_t macAddr[6];
+} struct_controller;
+
+typedef struct shooting_info
+{
+    uint8_t shooterID;
+    uint8_t senderMacAddr[6];
+} shooting_info;
+
+extern uint8_t GameStatus = 0;
+
+paringStatus ParingStatus = PAIRING;
+extern struct_controller movementInfo;
+extern struct_pairing paringInfoRec;
+extern struct_pairing paringInfo;
+extern shooting_info laserHit;
+uint8_t TankAddress[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+uint8_t ServerAddress[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+uint8_t ControlerAddress[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+const uint8_t BroadcastAddress[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+void GetMacAddress(uint8_t *DistAddress)
+{
+    String val = WiFi.macAddress();
+    Serial.println(val);
+    char *endPtr;
+    DistAddress[0] = strtol(val.c_str(), &endPtr, 16);
+    // read the first starting at the beginning of the buffer. this initializes endPtr as a pointer to the ':' after the first number
+    for (int i = 1; (*endPtr) && (i < 6); i++)
+        DistAddress[i] = strtol(endPtr + 1, &endPtr, 16);
+}
+
+
+
+#if PLATFORM == 8266
+// This is a tank
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+
+// Function prototypes
+/* Broadcast Function */
+void broadcast(const u8 *message, int stringLen);
+/* RecvCallback Function */
+void RecvCallbackGaming(uint8_t *senderMAC, uint8_t *incomingData, uint8_t len);
+/* SentCallback Function */
+void SentCallback(uint8_t *mac_addr, uint8_t sendStatus);
+/*report hit*/
+void ReportLaserHit(uint8_t shotterID);
+/*ParingController*/
+void Pairing(paringStatus targetParingStatus);
+/*get mac address*/
+void GetMacAddress(uint8_t *DistAddress);
+/*init espnow*/
+int espNowInit();
+
+int espNowInit()
+{
+    if (!esp_now_init())
+    {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+    Serial.print("ESP Board MAC Address:  ");
+    WiFi.mode(WIFI_STA);
+    Serial.println(WiFi.macAddress());
+    GetMacAddress(TankAddress);
+    esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
+    paringInfo.id = TANK;
+    memcpy(&(paringInfo.macAddr), TankAddress, 6);
+    memcpy(&(laserHit.senderMacAddr), TankAddress, 6);
+    return 0;
+}
+
+int espNowStart()
+{
+    esp_now_register_recv_cb(RecvCallbackPairing);
+    esp_now_register_send_cb(SentCallback);
+    Pairing(PAIRED_SERVER);
+    Pairing(PAIRED_CONTROLLER);
+    esp_now_unregister_recv_cb();
+    esp_now_register_recv_cb(RecvCallbackGaming);
+    return 0;
+}
+
+void Pairing(paringStatus targetParingStatus)
+{
+    // Broadcast the mac address
+    while (ParingStatus != targetParingStatus)
+    {
+        broadcast((u8 *)&paringInfo, sizeof(paringInfo));
+        while (callbackLock == 1)
+            delay(300);
+    }
+}
+
+void RecvCallbackPairing(uint8_t *senderMAC, uint8_t *incomingData, uint8_t len)
+{
+    if (callbackLock == 1)
+        return;
+    callbackLock = 1;
+    if (((struct_pairing *)incomingData)->id != TANK)
+    {
+        String senderMAC_s;
+        if (sscanf(senderMAC_s.c_str(), "%02X:%02X:%02X:%02X:%02X:%02X", &senderMAC[0], &senderMAC[1], &senderMAC[2], &senderMAC[3], &senderMAC[4], &senderMAC[5]) != 6)
+            Serial.println("ERROR while fetch mac address");
+        else
+            Serial.println("From " + senderMAC_s);
+        Serial.println("Got pairing information feedback!");
+        uint8_t *macPtr;
+        if (((struct_pairing *)incomingData)->id == CONTROLLER)
+            macPtr = ControlerAddress;
+        else if (((struct_pairing *)incomingData)->id == GAMESERVER)
+            macPtr = ServerAddress;
+        else
+        {
+           callbackLock = 0;
+           return; 
+        }
+        memcpy(macPtr, ((struct_pairing *)incomingData)->macAddr, 6);
+        esp_now_add_peer(macPtr, ESP_NOW_ROLE_COMBO, SIGNCHANNEL, NULL, 0);
+        // add the controller to the peer list
+        bool exists = esp_now_is_peer_exist(macPtr);
+        if (exists)
+            ParingStatus = (macPtr == ControlerAddress) ? PAIRED_CONTROLLER : PAIRED_SERVER;
+    }
+    callbackLock = 0;
+}
+
+void RecvCallbackGaming(uint8_t *senderMAC, uint8_t *incomingData, uint8_t len)
+{
+    String senderMAC_s;
+    if (sscanf(senderMAC_s.c_str(), "%02X:%02X:%02X:%02X:%02X:%02X", &senderMAC[0], &senderMAC[1], &senderMAC[2], &senderMAC[3], &senderMAC[4], &senderMAC[5]) != 6)
+        Serial.println("ERROR while fetching mac address");
+    else
+        Serial.println("From " + senderMAC_s);
+    switch (len)
+    {
+    case sizeof(struct_controller):
+        Serial.println("controller info from controller ");
+        Serial.println("Copying moveinfo");
+        memcpy(&movementInfo, incomingData, sizeof(struct_controller));
+        break;
+    case sizeof(uint8_t):
+        if (*incomingData == 1)
+        {
+            // stop moving
+        }
+        else if (*incomingData == 0)
+        {
+            // game start
+        }
+        break;
+    default:
+        Serial.println("ERROR");
+        break;
+    }
+}
+
+
+void ReportLaserHit(uint8_t shooterID)
+{
+    laserHit.shooterID = shooterID;
+    Serial.print("Hit by ");
+    // Broadcast a message to every device in range
+    int sendStatus = esp_now_send(ServerAddress, (u8 *)&laserHit, sizeof(shooting_info));
+    // Print results to serial monitor
+    if (sendStatus == 0)
+        Serial.println("Delivery success");
+    else
+        Serial.println("Delivery fail");
+    Serial.println();
+}
+
+void broadcast(const u8 *message, int stringLen)
+{
+    // Broadcast a message to every device in range
+    int sendStatus = esp_now_send(ServerAddress, (u8 *)message, stringLen);
+    // Print results to serial monitor
+    if (sendStatus == 0)
+        Serial.println("Delivery success");
+    else
+        Serial.println("Delivery fail");
+    Serial.println();
+}
+
+
+void SentCallback(const uint8_t *mac_addr, uint8_t sendStatus)
+{
+
+    Serial.print("Last Packet Send Status: ");
+    if (sendStatus == 0)
+        Serial.println("Delivery success");
+    else
+        Serial.println("Delivery fail");
+}
+
+
+#endif
+
+#if PLATFORM == 32
+#include <Arduino.h>
+#include <esp_now.h>
+#include <esp_wifi.h>
+#include <WiFi.h>
+
+uint8_t TankAddress[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+uint8_t ServerAddress[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+esp_now_peer_info_t slave;
+
+void broadcast(const uint8_t *message, int stringLen)
+{
+    // Broadcast a message to every device in range
+    int sendStatus = esp_now_send(ServerAddress, (uint8_t *)message, stringLen);
+    // Print results to serial monitor
+    if (sendStatus == 0)
+        Serial.println("Delivery success");
+    else
+        Serial.println("Delivery fail");
+    Serial.println();
+}
+
+
+int espNowInit()
+{
+    if (!esp_now_init())
+    {
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+    Serial.print("ESP Board MAC Address:  ");
+    WiFi.mode(WIFI_STA);
+    Serial.println(WiFi.macAddress());
+    GetMacAddress(ServerAddress);
+    esp_now_peer_info_t tanks;
+    paringInfo.id = GAMESERVER;
+    memcpy(&(paringInfo.macAddr), ServerAddress, 6);
+    return 0;
+}
+
+bool addPeer(const uint8_t *peer_addr)
+{ // add pairing
+    Serial.println("Pairing tank");
+    memset(&slave, 0, sizeof(slave));
+    const esp_now_peer_info_t *peer = &slave;
+    memcpy(slave.peer_addr, peer_addr, 6);
+    slave.channel = SIGNCHANNEL; // pick a channel
+    slave.encrypt = 0;           // no encryption
+    bool exists = esp_now_is_peer_exist(slave.peer_addr);
+    if (exists)
+    {
+        Serial.println("Already Paired");
+        return true;
+    }
+    else
+    {
+        esp_err_t addStatus = esp_now_add_peer(peer);
+        if (addStatus == ESP_OK)
+        {
+            Serial.println("Pair success");
+            return true;
+        }
+        else
+        {
+            Serial.println("Pair failed");
+            return false;
+        }
+    }
+}
+
+int espNowStartPairing()
+{
+    esp_now_register_recv_cb(RecvCallbackPairing);
+    esp_now_register_send_cb(SentCallback);
+    return 0;
+}
+
+
+int espNowStopPairing()
+{
+     esp_now_unregister_recv_cb();
+     return 0;
+}
+
+
+
+void RecvCallbackPairing(const uint8_t *senderMAC, const uint8_t *incomingData, int len)
+{
+
+    String senderMAC_s;
+    if (sscanf(senderMAC_s.c_str(), "%02X:%02X:%02X:%02X:%02X:%02X", &senderMAC[0], &senderMAC[1], &senderMAC[2], &senderMAC[3], &senderMAC[4], &senderMAC[5]) != 6)
+        Serial.println("ERROR while fetching mac address");
+    else
+        Serial.println("From " + senderMAC_s);
+    switch (len)
+    {
+    case sizeof(struct_pairing):
+        if (((struct_pairing *)incomingData)->id == TANK)
+        {
+            Serial.println("Paring info from tank");
+            Serial.println("Copying tankinfo");
+            memcpy(&paringInfoRec, incomingData, sizeof(struct_pairing));
+            addPeer(paringInfoRec.macAddr);
+            Serial.println("Sending response");
+            esp_err_t result = esp_now_send(paringInfoRec.macAddr, (uint8_t *) &paringInfo, sizeof(paringInfo));
+        }
+        return;
+    default:
+        Serial.println("Nothing to do");
+        return;
+    }
+}
+
+void GameManager(bool gameStatus)
+{
+    if(gameStatus)
+    {
+        GameStatus = 1;
+        broadcast(&GameStatus,1);
+    }    
+    else
+    {
+        GameStatus = 0;
+        broadcast(&GameStatus,1);
+    }
+    return;
+}
+void SentCallback(const uint8_t *mac_addr,  esp_now_send_status_t sendStatus)
+{
+
+    Serial.print("Last Packet Send Status: ");
+    if (sendStatus == 0)
+        Serial.println("Delivery success");
+    else
+        Serial.println("Delivery fail");
+}
+#endif
+#endif
