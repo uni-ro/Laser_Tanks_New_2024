da9e1583c9bd62da88af6429b7d44b69c87f8162
uni-ro <140714461+uni-ro@users.noreply.github.com>
2026-08-27 20:09:30 +0800

    Updated Dual Core Code to use Trimpot Threshold

--- Code/src/Dual_Core/ESP-Dual-Core.ino
+++ Code/src/Dual_Core/ESP-Dual-Core.ino
@@ -3,6 +3,14 @@
 void coreTaskZero(void * pvParameters);
 void coreTaskOne(void * pvParameters);
 
+uint8_t LASER1 = 18;
+uint8_t LASER2 = 17;
+uint8_t PHOTO = 15; //Phototransistor
+uint8_t THRESHPIN = 35;
+
+// thresh could be 560
+int detectionThresh = 300;
+
 void setup()
 {
   // put your setup code here, to run once:
@@ -36,11 +44,13 @@ void loop()
 
 void coreTaskZero(void * pvParameters)
 {
-  pinMode(18, OUTPUT);
+  pinMode(LASER1, OUTPUT);
+  pinMode(LASER2, OUTPUT);
 
   while(1)
   {
-    analogWrite(18,100);
+    analogWrite(LASER1,100);
+    analogWrite(LASER2,200);
     
     // for (int i = 0; i < 1000; i++)
     // {
@@ -49,6 +59,8 @@ void coreTaskZero(void * pvParameters)
     //   digitalWrite(18, LOW);
     //   delayMicroseconds(1000 - i);
     // }
+
+    detectionThresh = analogRead(THRESHPIN);
     
     vTaskDelay(pdMS_TO_TICKS(1));
   }
@@ -63,14 +75,22 @@ void coreTaskOne(void * pvParameters)
   // int prevRead = 0;
   // int currRead = 0;
 
-  int thresh = 15;
+  // int thresh = 15;
+  // int thresh = 2000;
+  // int thresh = 300;
   // unsigned long startTime = 0, endTime = 0;
   unsigned long lastPrint = 0;
 
   int doPrint = 0;
 
+  int currTime = 0, prevTime = 0;
+
+  uint16_t maxRead = 0;
+
   while(1)
   {
+    // printf("Detection Threshold is: %d\n", detectionThresh);
+
     // int res1 = analogRead(15);
     // int res2 = analogRead(2);
     // int res3 = analogRead(4);
@@ -119,39 +139,61 @@ void coreTaskOne(void * pvParameters)
 
     // prevRead = currRead;
 
-    int read = analogRead(15);
+    int read = analogRead(PHOTO);
+    currTime = millis();
 
-    if (read >= thresh)
+    // printf("Read value is: %d\n", read);
+
+    if (read > maxRead)
+    {
+      maxRead = read;
+    }
+
+    if (read >= detectionThresh)
     {
       i++;
     }
-    else
-    {
-      // j++;
+    // else
+    // {
+    //   // j++;
 
-      if (i != 0)
-      {
-        avgI += i;
-        j++;
+    //   if (i != 0)
+    //   {
+    //     avgI += i;
+    //     j++;
         
-        if (millis() - lastPrint >= 100)
-        {
-          printf("Number of iter: %d\n", avgI / j);
-          lastPrint = millis();
+    //     if (millis() - lastPrint >= 100)
+    //     {
+    //       // printf("Ambient: %d\t", read);
+    //       printf("Number of iter: %d\n", avgI / j);
+    //       lastPrint = millis();
           
-          avgI = 0;
-          j = 0;
-        }
-        // doPrint = 0;
-      }
+    //       avgI = 0;
+    //       j = 0;
+    //     }
+    //     // doPrint = 0;
+    //   }
 
-      // if (j >= 1000)
-      // {
-      //   doPrint = 1;
-      //   j = 0;
-      // }
+    //   // if (j >= 1000)
+    //   // {
+    //   //   doPrint = 1;
+    //   //   j = 0;
+    //   // }
+
+    //   i = 0;
+    // }
+
+    if (i > 0 && currTime - prevTime >= 100)
+    {
+      // Duty cycle equivalent
+      uint32_t dc = i / (currTime - prevTime);
+
+      printf("Ambient read: %d & thresh: %d & i: %d & maxRead: %d\t", read, detectionThresh, i, maxRead);
+      printf("DC equiv.: %d\n", dc);
 
       i = 0;
+      prevTime = millis();
+      maxRead = 0;
     }
   }
 }
